4bd13166617baa48138468ac92669a25dbc5b5d4
Bruno Kohn <brunokohn1993@gmail.com>
2023-01-09 22:44:45 -0300

    feat: multiplica ai exercise

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Tabela de multiplicação de 7");
+       for(int i = 1; i <= 10; i++) {
+        final int myNumber = 7;
+        System.out.println("7 * " + i + " = " +  myNumber * i);
+       }
+    }
+}
